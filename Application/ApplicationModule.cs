62ad7506846a796ce3a4f7fcf85bfeac2222c0cf
k-felipe <felipegkubiak@outlook.com>
2025-01-21 23:38:23 -0300

    Remoção dos project services.

--- Application/ApplicationModule.cs
+++ Application/ApplicationModule.cs
@@ -15,7 +15,6 @@ namespace DevFreela.Application
 
         private static IServiceCollection AddServices(this IServiceCollection services)
         {
-            services.AddScoped<IProjectService, ProjectService>();
             services.AddScoped<ISkillService, SkillService>();
             services.AddScoped<IUserService, UserService>();
             return services;
